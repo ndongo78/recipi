72e5fd9ddd48eb33b60a5134113c4e3669d593b9
ndongo78 <ndongodahs@yahoo.fr>
2023-05-14 00:12:00 +0200

    adding screen

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ import {menuItems} from "../js/menu.js"
 
 $(document).ready(function() {
     $("#about").load("../components/about.html")
-    //$("#menu").load("../components/menu.html")
+    $("#reservaton").load("../components/reservation.html")
+   // $("#avis").load("../components/avis.html")
   $(".detail").slideDown();
    $("#menuBtn").click(function (e) {
       $("#menuLinks li").toggle()
@@ -32,30 +33,57 @@ menuItems.forEach(item=>render(item))
  function render(item){
   const start=getStarsFromRating(item.note);
 
-   logger(start)
-   
-   const template=`<div class="   mt-8 shadow-xl bg-[#222] mr-3 w-[220px]">
-    <img src=${item.image} alt=${item.title} class="cardImage w-full h-32 object-cover rounded"/>
-    <div class="bg-black w-full mt-2">
-    <h2 class="text-[#ccc] mt-3"> ${item.title} </h2>
-    <div class="flex flex-row mt-3 mb-3 w-full">
-    <i class="material-icons text-[#ccc]">access_time</icon>
- <p class="text-yellow-600">15mn</p>
-    </div>
-    <div class="w-full">
-    <p class="text-[#ccc]">${start}</p>
-    <div class="text-white">Commander</div>
-    </div>
-    </div>
-   </div>`;
 
 
- //div.css("background-color","red")
+     const template = `<div class=" shadow-xl bg-[#444] animate  w-[220px] m-6 rounded">
+  <img src=${item.image} alt=${item.title} class="cardImage w-full h-32 object-cover rounded"/>
+  <div class="w-full mt-2">
+    <h2 class="text-[#ccc] mt-3 ml-2 text-2xl"> ${item.title} </h2>
+    <div class="flex flex-row mt-3 mb-3 ml-2 w-full items-center">
+      <i class="material-icons text-[#ccc]">access_time</i>
+      <p class="text-[#ccc] text-xl ml-3">15mn</p>
+    </div>
+    <div class="w-full flex justify-between m-2">
+      <p class="text-[#ccc]">${start}</p>
+      <a class="text-xl mr-3 text-yellow-500 cursor-pointer">commander</a>
+    </div>
+  </div>
+</div>`;
 
-   $("#cardContainer").append(template)
+     const container = $("#cardContainer");
+     const card = $(template);
+     container.append(card); // Ajouter la carte au conteneur
+
  }
-  
 
+const crasouselItems=$(".carousel").children;
+const items=document.querySelector(".carousel").children
+
+
+let currentIndex=0;
+$("#btn1").click((e)=>{
+ const index=$("#btn1").attr("tabindex")
+    for (let i=0; i<items.length; i++){
+        items[i].classList.remove("active")
+    }
+    items[index].classList.add("active")
+})
+$("#btn2").click((e)=>{
+ const index=$("#btn2").attr("tabindex")
+    for (let i=0; i<items.length; i++){
+        items[i].classList.remove("active")
+    }
+    items[index].classList.add("active")
+})
+$("#btn3").click((e)=>{
+ const index=$("#btn3").attr("tabindex")
+    for (let i=0; i<items.length; i++){
+        items[i].classList.remove("active")
+    }
+    items[index].classList.add("active")
+})
+
+$('.item').addClass('moveFromRight');
 
 $(window).scroll(function() {
    var scrollTop = $(this).scrollTop();
@@ -63,9 +91,11 @@ $(window).scroll(function() {
       $('.descript').addClass('animate');
       $(".imgContainer img").addClass('animate2')
       $(".descript").addClass('moveFromRight')
+      $("#cardContainer").addClass('moveFromBottom')
    } else {
       $('.descript').removeClass('animate');
       $('.imgContainer img').removeClass('animate2');
       $('.descript').removeClass('moveFromRight');
+      $('#cardContainer').removeClass('moveFromBottom');
    }
 });
